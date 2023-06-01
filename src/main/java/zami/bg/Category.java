b81671ffc14274a1c7d5840da9de425e89f38a17
Marian <marian.klecherov65@gmail.com>
2023-06-01 14:56:01 +0300

    reading all the categories

--- src/main/java/zami/bg/Category.java
+++ src/main/java/zami/bg/Category.java
@@ -3,14 +3,18 @@ package zami.bg;
 public class Category {
     public String name = "";
     public String urlAddress = "";
-    public String parent = "this category is the parent category";
+    public Category parent = null;
+
+    public Category(String name) {
+        this.name = name;
+    }
 
     public Category(String name, String urlAddress) {
         this.name = name;
         this.urlAddress = urlAddress;
     }
 
-    public Category(String name, String urlAddress, String parent) {
+    public Category(String name, String urlAddress, Category parent) {
         this.name = name;
         this.urlAddress = urlAddress;
         this.parent = parent;
@@ -32,11 +36,11 @@ public class Category {
         this.urlAddress = urlAddress;
     }
 
-    public String getParent() {
+    public Category getParent() {
         return parent;
     }
 
-    public void setParent(String parent) {
+    public void setParent(Category parent) {
         this.parent = parent;
     }
 
--- src/main/java/zami/bg/Main.java
+++ src/main/java/zami/bg/Main.java
@@ -6,38 +6,73 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         Document doc = Jsoup.connect("https://zani.bg/").get();
-        Elements categoriesNames = doc.select("#menu-classic-shop-main-menu >li a .menu-text");
-        Elements coCategoriesNames = doc.select("#menu-classic-shop-main-menu >li .sub-menu >li a");
-        Elements lis = doc.getElementById("menu-classic-shop-main-menu").getElementsByTag("li");
+        Elements mainMenuLis = doc.getElementById("menu-classic-shop-main-menu").getElementsByTag("li");
         List<String> names = new ArrayList<>();
-        for (Element li : lis) {
+        PrintStream fileWriter = new PrintStream("export-of-categories.txt");
+        List<String> podCategories = null;
+        List<Category> categoryList = new ArrayList<>();
+        List<Category> parentCategoryList = new ArrayList<>();
+        for (Element li : mainMenuLis) {
             Element span = li.getElementsByClass("menu-text").first();
+            Element url = li.getElementsByAttribute("href").first();
             if (span != null) {
                 names.add(span.text().trim());
+                Category category = new Category(span.text().trim(), url.attr("href"));
+                categoryList.add(category);
+                parentCategoryList.add(category);
             }
-        }
-        List<String> catUrls = new ArrayList<>();
-        int index = 0;
-        doc.select("#menu-classic-shop-main-menu >li").get(1).select("a").attr("href");
+            Element subMenu = li.getElementsByTag("ul").first();
+            if (subMenu == null) {
+                //fileWriter.println(parentCategoryList.get(parentCategoryList.size()-1));
+                //fileWriter.println(categoryList.get(categoryList.size() -1));
+                continue;
+            }
+            Elements subMenuList = subMenu.getElementsByTag("li");
+            for (Element li1 : subMenuList) {
+                Element spanSub = li1.getElementsByTag("span").first();
+                Element subUrl = li1.getElementsByAttribute("href").first();
+                if (spanSub == null) {
+                    continue;
+                }
+                podCategories = new ArrayList<>();
+                podCategories.add(spanSub.text().trim());
+                Category category = new Category(podCategories.get(0), subUrl.attr("href"), parentCategoryList.get(parentCategoryList.size() - 1));
+                categoryList.add(category);
+                //fileWriter.println(category);
+            }
 
-        for (int i = 0; i < categoriesNames.size(); i++) {
-            catUrls.add(doc.select("#menu-classic-shop-main-menu >li").get(i).select("a").attr("href"));
-            Category category = new Category(names.get(i), catUrls.get(i));
-            System.out.println(category.toString());
         }
-        System.out.println(names);
-    }
-    private static void log(String title) {
-        System.out.println(title);
-    }
-
-    private static void log(String s, String title, String href) {
-        System.out.println(title);
+        for (int i = 0; i < categoryList.size(); i++) {
+            fileWriter.println(categoryList.get(i));
+        }
+//        List<String> catUrls = new ArrayList<>();
+//        List<String> childCatUrls = new ArrayList<>();
+//        doc.select("#menu-classic-shop-main-menu >li").get(1).select("a").attr("href");
+//        for (int i = 0; i < parentCategoriesNames.size(); i++) {
+//            catUrls.add(doc.select("#menu-classic-shop-main-menu >li").get(i).select("a").attr("href"));
+//            Category category = new Category(names.get(i), catUrls.get(i));
+//            fileWriter.println(category.toString());
+//        }
+//
+//        while (index < parentCategoriesNames.size()) {
+//            while(index < doc.getElementsByClass("sub-menu").get(index).childrenSize()) {
+//          Element span = doc.getElementsByClass("sub-menu").get(index).getElementsByTag("span").first();
+//                childCatUrls.add(doc.select("#menu-classic-shop-main-menu >li .sub-menu >li").get(index).select("a").attr("href"));
+//                podCategories = doc.getElementsByClass("sub-menu").get(index).getElementsByTag("span").eachText();
+//               Category category1 = new Category(podCategories.get(index), childCatUrls.get(index), names.get(index + 1));
+//                fileWriter.println(category1);
+//                index++;
+//            }
+//            index++;
+//        }
+//        System.out.println(names);
+//    }
     }
 }
