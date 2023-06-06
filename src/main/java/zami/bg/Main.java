0827cdf64be2e9e95809f62ba213cb46a70a1704
Marian <marian.klecherov65@gmail.com>
2023-06-06 14:10:49 +0300

    readProducts from every podCategory

--- src/main/java/zami/bg/Main.java
+++ src/main/java/zami/bg/Main.java
@@ -10,6 +10,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class Main {
@@ -65,34 +66,18 @@ public class Main {
         Elements tableLis = podDoc.getElementsByClass("products clearfix products-4");
         Elements lis = tableLis.select("li");
         int index = 1;
-        String stringIndex = "1";
+        //String stringIndex = "1";
         List<Product> products = new ArrayList<>();
         if (podDoc.getElementsByClass("next page-numbers").first() == null) {
-            System.out.println("Now going to " + url + "/page/" + stringIndex + "/");
-            podDoc = Jsoup.connect(url + "page/" + stringIndex + "/").get();
-            tableLis = podDoc.getElementsByClass("products clearfix products-4");
-            lis = tableLis.select("li");
-            stringIndex = String.valueOf(index);
-            fileWriter.println(category.getName());
-            for (Element li : lis) {
-                Element podUrl = li.getElementsByAttribute("href").first();
-                Document document = Jsoup.connect(podUrl.attr("href")).get();
-                Element summaryContainer = document.getElementsByClass("summary-container").first();
-                String name = summaryContainer.getElementsByAttributeValue("itemprop", "name").first().text();
-                String price = summaryContainer.getElementsByClass("price").first().text();
-                Product product = new Product(name, price, category);
-                System.out.println(product);
-                fileWriter.println(product);
-                products.add(product);
-            }
+            readProduct(url + "page/" + index + "/", fileWriter, category);
         }
         while(podDoc.getElementsByClass("next page-numbers").first() != null) {
-            System.out.println("Now going to " + url + "/page/" + stringIndex + "/");
-            podDoc = Jsoup.connect(url + "page/" + stringIndex + "/").get();
+            System.out.println("Now going to " + url + "/page/" + index + "/");
+            podDoc = Jsoup.connect(url + "page/" + index + "/").get();
             tableLis = podDoc.getElementsByClass("products clearfix products-4");
             lis = tableLis.select("li");
             index++;
-            stringIndex = String.valueOf(index);
+            //stringIndex = String.valueOf(index);
             fileWriter.println(category.getName());
             for (Element li : lis) {
                 Element podUrl = li.getElementsByAttribute("href").first();
@@ -100,14 +85,52 @@ public class Main {
                 Element summaryContainer = document.getElementsByClass("summary-container").first();
                 String name = summaryContainer.getElementsByAttributeValue("itemprop", "name").first().text();
                 String price = summaryContainer.getElementsByClass("price").first().text();
+                Elements imageTableLis = document.getElementsByClass("ol");
+                Elements iLis = imageTableLis.select("li");
+
+                List<String> images = new ArrayList<>();
+                for (Element li1 : iLis) {
+                    Element element = document.getElementsByClass("avada-single-product-gallery-wrapper avada-product-images-global avada-product-images-thumbnails-bottom").first().getElementsByTag("img").first();
+                    images.add(element.text());
+                }
+
                 Product product = new Product(name, price, category);
                 System.out.println(product);
                 fileWriter.println(product);
                 products.add(product);
+                //sleep(800);
             }
-            fileWriter.println("Next page");
+            fileWriter.println("Now going to " + url + "/page/" + index + "/");
         }
-
+        System.out.println(products.size());
+    }
+    public static void sleep(long millis){
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+    public static List<Product> readProduct(String url, PrintStream fileWriter, Category category) throws IOException {
+        List<Product> products = new ArrayList<>();
+        Document podDoc = Jsoup.connect(url).get();
+        Elements tableLis = podDoc.getElementsByClass("products clearfix products-4");
+        Elements lis = tableLis.select("li");
+        //stringIndex = String.valueOf(index);
+        fileWriter.println(category.getName());
+        for (Element li : lis) {
+            Element podUrl = li.getElementsByAttribute("href").first();
+            Document document = Jsoup.connect(podUrl.attr("href")).get();
+            Element summaryContainer = document.getElementsByClass("summary-container").first();
+            String name = summaryContainer.getElementsByAttributeValue("itemprop", "name").first().text();
+            String price = summaryContainer.getElementsByClass("price").first().text();
+            Product product = new Product(name, price, category);
+            System.out.println(product);
+            fileWriter.println(product);
+            products.add(product);
+            //sleep(800);
+        }
+        return products;
     }
 }
 
--- src/main/java/zami/bg/Product.java
+++ src/main/java/zami/bg/Product.java
@@ -1,9 +1,70 @@
 package zami.bg;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class Product {
     public String name = "";
     public String price = "";
     public Category category = null;
+    public List<String> images = new ArrayList<>();
+    public String catalogNumber = "";
+    public HashMap<String , String> features = new HashMap<>();
+    public String description = "";
+    public BigDecimal promoPrice = null;
+
+    public Product(String name, String price, Category category, List<String> images, String catalogNumber, HashMap<String, String> features, String description, BigDecimal promoPrice) {
+        this.name = name;
+        this.price = price;
+        this.category = category;
+        this.images = images;
+        this.catalogNumber = catalogNumber;
+        this.features = features;
+        this.description = description;
+        this.promoPrice = promoPrice;
+    }
+
+    public List<String> getImages() {
+        return images;
+    }
+
+    public void setImages(List<String> images) {
+        this.images = images;
+    }
+
+    public String getCatalogNumber() {
+        return catalogNumber;
+    }
+
+    public void setCatalogNumber(String catalogNumber) {
+        this.catalogNumber = catalogNumber;
+    }
+
+    public HashMap<String, String> getFeatures() {
+        return features;
+    }
+
+    public void setFeatures(HashMap<String, String> features) {
+        this.features = features;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getPromoPrice() {
+        return promoPrice;
+    }
+
+    public void setPromoPrice(BigDecimal promoPrice) {
+        this.promoPrice = promoPrice;
+    }
 
     public Product(String name, Category category) {
         this.name = name;
